6033c274f872f0b02a4c4b83690c40796965a360
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-10-05 17:01:24 -0300

    .env creation for deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  apps:[{
-    name:"app",
-    script:"./src/server.js",
-    instances:"max",
-    env:{
-      NODE_ENV: "developement",
+  apps : [{
+    name: "app",
+    script: "./src/server.js",
+    instances: "max",
+
+
+    env: {
+      NODE_ENV: "development",
     },
     env_production: {
-      NODE_ENV:"production",
+      NODE_ENV: "production",
     }
   }]
-    
   
-};
+}
--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     jwt: {
         secret: process.env.AUTH_SECRET || "default",
-        expiresIn: "1d"
-    }
-}
+        expiresIn: "1d",
+    },
+};
