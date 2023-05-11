b214e659c80cbffd80bc25f7b47bfe54a44eb65a
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-05-11 19:08:01 -0300

    Corrigindo Deploy

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
-      message: error.message
+    message: error.message
     });
   };
 
